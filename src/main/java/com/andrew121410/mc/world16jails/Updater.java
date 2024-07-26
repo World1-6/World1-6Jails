252a409f490ba7b92ef6126c96f17e0e027186af
Andrew121410 <andrewcherring@gmail.com>
2024-07-25 23:18:48 -0400

    Change to World16HashBasedUpdater

--- src/main/java/com/andrew121410/mc/world16jails/Updater.java
+++ src/main/java/com/andrew121410/mc/world16jails/Updater.java
@@ -1,8 +1,8 @@
 package com.andrew121410.mc.world16jails;
 
-import com.andrew121410.mc.world16utils.utils.ccutils.utils.HashBasedUpdater;
+import com.andrew121410.mc.world16utils.updater.World16HashBasedUpdater;
 
-public class Updater extends HashBasedUpdater {
+public class Updater extends World16HashBasedUpdater {
 
     private static final String JAR_URL = "https://github.com/World1-6/World1-6Jails/releases/download/latest/World1-6Jails.jar";
     private static final String HASH_URL = "https://github.com/World1-6/World1-6Jails/releases/download/latest/hash.txt";
